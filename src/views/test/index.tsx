5a5d4f46f70520fd7ff4e370783f4d7d26f66839
duhuanwen <duhuanwen@163.com>
2024-03-28 17:26:37 +0800

    复习space两级传递值

--- src/views/test/index.tsx
+++ src/views/test/index.tsx
@@ -1,35 +1,25 @@
-import React, { useRef } from "react";
-import Message from "../practice/message copy";
-import { MessageRef } from "../practice/message copy";
-import useMessage from "../practice/message copy/hooks/useMessageTest";
-import { MessageProvider } from "../practice/message copy/configProvider";
+import { useState } from "react";
+import Sapce from "../practice/space basic";
+import { SpaceProvider } from "../practice/space basic/context";
 
 function Test() {
-  const message = useMessage();
-  let count = 1;
-  const messageRef = useRef<MessageRef>(null);
-  const handleChange = () => {
-    messageRef.current?.changeNum(count++);
-  };
-  const handleChange2 = () => {
-    message?.changeNum(count++);
+  const [size, setSize] = useState("init");
+  const changeSize = () => {
+    setSize("big");
   };
   return (
     <>
       test
       {/* <br />
       <button>更新</button> */}
-      <button onClick={handleChange}>修改1111</button>
-      <button onClick={handleChange2}>修改2222</button>
+      <button onClick={changeSize}>改变size</button>
       <br />
-      <Message ref={messageRef} num={10}></Message>
+      <SpaceProvider space={{ size }}>
+        <Sapce />
+      </SpaceProvider>
       <br />
     </>
   );
 }
-const Test2 = () => (
-  <MessageProvider>
-    <Test></Test>
-  </MessageProvider>
-);
-export default Test2;
+
+export default Test;
