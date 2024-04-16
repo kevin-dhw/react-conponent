1a6083af52feaac715096787f3bf26a1d3dbbdbb
duhuanwen <duhuanwen@163.com>
2024-04-16 17:44:53 +0800

    spring

--- src/views/test/index.tsx
+++ src/views/test/index.tsx
@@ -1,54 +1,12 @@
-import { FC } from "react";
-import { Button, Checkbox, Input } from "antd";
-import Form from "../Form/form2";
+import React from "react";
+import Spring from "../../views/practice/spring";
 
-const Test: FC = () => {
-  // const formRef = useRef(null);
-  const onFinish = (values: any) => {
-    console.log("Success:", values);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
-  };
+const Test: React.FC = () => {
   return (
     <>
-      <Form
-        initialValues={{ remember: true, username: "神说要有光" }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            { required: true, message: "请输入用户名!" },
-            { max: 6, message: "长度不能大于 6" },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "请输入密码!" }]}
-        >
-          <Input.TextArea />
-        </Form.Item>
-
-        <Form.Item name="remember" valuePropName="checked">
-          <Checkbox>记住我</Checkbox>
-        </Form.Item>
-
-        <Form.Item>
-          <div>
-            <Button type="primary" htmlType="submit">
-              登录
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
+      Spring
+      <br />
+      <Spring />
     </>
   );
 };
