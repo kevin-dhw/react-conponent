e8779162b8b0f99ab4345de3657757693d98cd0a
kevin <932731452@qq.com>
2025-07-16 16:11:57 +0800

    icon

--- src/views/test/component/icon/index.tsx
+++ src/views/test/component/icon/index.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import { createFromIconfont } from "./Icon/createFrontIconfont";
+import { IconAdd } from "./Icon/icons/IconAdd";
+import { IconEmail } from "./Icon/icons/IconEmail";
+
+const IconFont = createFromIconfont(
+  "//at.alicdn.com/t/c/font_4443338_a2wwqhorbk4.js"
+);
 
 const Icon: React.FC = () => {
+  const ref = useRef<SVGSVGElement>(null);
+  const ref2 = useRef<SVGSVGElement>(null);
   return (
     <div>
-      Icon
-      <span></span>
+      <div style={{ padding: "50px" }}>
+        <IconAdd size="40px" ref={ref}></IconAdd>
+        <IconEmail spin></IconEmail>
+        <IconEmail style={{ color: "blue", fontSize: "50px" }}></IconEmail>
+      </div>
+      <div style={{ padding: "50px" }}>
+        <IconFont type="icon-shouye-zhihui" size="40px" ref={ref2}></IconFont>
+        <IconFont
+          type="icon-gerenzhongxin-zhihui"
+          fill="blue"
+          size="40px"
+        ></IconFont>
+      </div>
     </div>
   );
 };
